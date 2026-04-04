8a202cd658cc581a84dc96470a9ba30fc83b3984
atoccaoficial <atoccaoficial@gmail.com>
2026-04-04 20:23:20 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
   url: "https://leypzjulxskqhxzuijjb.supabase.co",
-  anonKey: "sb_publishable_KFdDoU5eXIwtU5wAIK_M0g_qVKqs",
+  anonKey: "sb_publishable_KFdDoU5eXIwtU5wAIK_M0g_qVKqs"
 };
